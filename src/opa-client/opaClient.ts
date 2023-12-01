57e7d87cc7226474f5e1a4b3b3ef6182122273ac
Peter Macdonald <macdonald.peter90@gmail.com>
2023-12-01 21:02:21 +0100

    node-fetch
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- src/opa-client/opaClient.ts
+++ src/opa-client/opaClient.ts
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 import { Config } from '@backstage/config';
 import { Logger } from 'winston';
 import { PolicyEvaluationInput } from '../types';
+import { ResponseError } from '@backstage/errors';
 
 export class OpaClient {
   private readonly baseUrl: string;
@@ -17,21 +18,33 @@ export class OpaClient {
       `Sending request to OPA: ${this.baseUrl}/api/opa/opa-permissions`,
     );
 
-    this.logger.info(`Sending request to OPA: ${JSON.stringify(input)}`);
+    this.logger.info(`Sending input to OPA: ${JSON.stringify(input)}`);
 
     try {
-      const response = await axios.post(
+      const response = await fetch(
         `${this.baseUrl}/api/opa/opa-permissions`,
         {
-          policyInput: input,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            policyInput: input,
+          }),
         },
       );
 
+      if (!response.ok) {
+        throw await ResponseError.fromResponse(response);
+      }
+
+      const data = await response.json();
+
       this.logger.info(
-        `Received response from OPA server: ${JSON.stringify(response.data)}`,
+        `Received response from OPA server: ${JSON.stringify(data)}`,
       );
 
-      return response.data;
+      return data;
     } catch (error: unknown) {
       this.logger.error('Error during OPA policy evaluation:', error);
       throw new Error(`Failed to evaluate policy: ${error}`);
